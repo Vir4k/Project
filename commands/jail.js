75cc43e368baccca350b93dd271de0f762381e5d
VirakTV <67989471+VirakTV@users.noreply.github.com>
2020-08-04 10:29:37 +0700

    Create jail.js

---
+++ commands/jail.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const bot = require('../config.json');
+const canva = require('canvacord');
+let newCanva = new canva();
+
+module.exports.run = async (bot, message, args) => {
+  
+
+   let mention = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+   
+   let ifMention;
+   if(mention){
+     ifMention = mention.user;
+   } else {
+     ifMention = message.author;
+   }
+   
+   let jailed = await newCanva.jail(ifMention.displayAvatarURL({
+     dynamic: true,
+     size: 2048,
+     format: 'png'
+   }));
+   
+   return message.channel.send({ files: [{ attachment: jailed, name: "Jail.png"}] });
+}
+
+module.exports.config = {
+  name: "jail",
+  description: "Shows an image of a user's avatar behind bars",
+  usage: `${bot.prefix}jail [User]`,
+  accessedBy: "member",
+  aliases: ['catch']
+}
